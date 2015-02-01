6b22ca57ac5842adc20db30ed6b452b61aed4d3a
huoex <huoex0628@gmail.com>
2015-02-02 06:15:35 +0800

    bean.xml testContext.xml
    Signed-off-by: huoex <huoex0628@gmail.com>

---
+++ src/main/java/leon/ssi/test/friend.java
@@ -0,0 +1,19 @@
+package leon.ssi.test;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class friend {
+	
+	String name ="lz";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = "lz";
+	}
+	
+
+}
---
+++ src/main/java/leon/ssi/test/person.java
@@ -0,0 +1,12 @@
+package leon.ssi.test;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class person {
+	@Resource
+	friend frd;
+
+}
