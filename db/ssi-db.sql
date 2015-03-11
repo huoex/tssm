a67c81c1742a628d2d03526fcdf7c3ecef30ba64
huoex <huoex0628@gmail.com>
2015-03-11 09:52:43 +0800

    ajax
    Signed-off-by: huoex <huoex0628@gmail.com>

--- db/ssi-db.sql
+++ db/ssi-db.sql
@@ -16,6 +16,19 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`ssi` /*!40100 DEFAULT CHARACTER SET utf
 
 USE `ssi`;
 
+/*Table structure for table `course` */
+
+DROP TABLE IF EXISTS `course`;
+
+CREATE TABLE `course` (
+  `cid` varchar(10) DEFAULT NULL,
+  `cname` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `course` */
+
+insert  into `course`(`cid`,`cname`) values ('1','java');
+
 /*Table structure for table `student` */
 
 DROP TABLE IF EXISTS `student`;
@@ -30,7 +43,21 @@ CREATE TABLE `student` (
 
 /*Data for the table `student` */
 
-insert  into `student`(`sid`,`sname`,`age`,`gender`) values ('1','abc',23,'1');
+insert  into `student`(`sid`,`sname`,`age`,`gender`) values ('1','abc',23,'1'),('2','def',24,'2'),('3','eee',25,'3'),('4','fff',26,'4'),('6','zzz',10,'5');
+
+/*Table structure for table `student_course` */
+
+DROP TABLE IF EXISTS `student_course`;
+
+CREATE TABLE `student_course` (
+  `cid` varchar(10) DEFAULT NULL,
+  `sid` varchar(10) DEFAULT NULL,
+  `score` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `student_course` */
+
+insert  into `student_course`(`cid`,`sid`,`score`) values ('1','1','90');
 
 /*Table structure for table `t_role` */
 
@@ -45,6 +72,17 @@ CREATE TABLE `t_role` (
 /*Data for the table `t_role` */
 
 insert  into `t_role`(`id`,`role_name`) values (1,'abc');
+
+/*Table structure for table `t_student` */
+
+DROP TABLE IF EXISTS `t_student`;
+
+CREATE TABLE `t_student` (
+  `student_id` varchar(10) DEFAULT NULL,
+  `student_name` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_student` */
 
 /*Table structure for table `t_user` */
 
@@ -62,7 +100,20 @@ CREATE TABLE `t_user` (
 
 insert  into `t_user`(`id`,`username`,`password`,`role_id`) values ('1','abc','abc',1);
 
+/*Table structure for table `teacher` */
+
+DROP TABLE IF EXISTS `teacher`;
+
+CREATE TABLE `teacher` (
+  `tid` varchar(10) DEFAULT NULL,
+  `tname` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `teacher` */
+
+insert  into `teacher`(`tid`,`tname`) values ('1','Lis');
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;/*Table structure for table `t_student` */DROP TABLE IF EXISTS `t_student`;CREATE TABLE `t_student` (  `student_id` varchar(10) DEFAULT NULL,  `student_name` varchar(30) DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
